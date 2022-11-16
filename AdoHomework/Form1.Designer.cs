b2ae913ae4a56e14a3348e0ff2e723c54e4f015b
nikhildesai6666 <desainikhil>
2022-11-16 21:00:14 +1030

    Add project files.

---
+++ AdoHomework/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace AdoHomework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.txtrollno = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtcity = new System.Windows.Forms.TextBox();
+            this.txtpercentage = new System.Windows.Forms.TextBox();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.btnshowall = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Roll No";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "City";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Percentage";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(51, 195);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 4;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // txtrollno
+            // 
+            this.txtrollno.Location = new System.Drawing.Point(107, 15);
+            this.txtrollno.Name = "txtrollno";
+            this.txtrollno.Size = new System.Drawing.Size(100, 20);
+            this.txtrollno.TabIndex = 5;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(107, 53);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(100, 20);
+            this.txtname.TabIndex = 6;
+            // 
+            // txtcity
+            // 
+            this.txtcity.Location = new System.Drawing.Point(107, 87);
+            this.txtcity.Name = "txtcity";
+            this.txtcity.Size = new System.Drawing.Size(100, 20);
+            this.txtcity.TabIndex = 7;
+            // 
+            // txtpercentage
+            // 
+            this.txtpercentage.Location = new System.Drawing.Point(107, 125);
+            this.txtpercentage.Name = "txtpercentage";
+            this.txtpercentage.Size = new System.Drawing.Size(100, 20);
+            this.txtpercentage.TabIndex = 8;
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(154, 195);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(75, 23);
+            this.btnupdate.TabIndex = 9;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.Location = new System.Drawing.Point(253, 195);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(75, 23);
+            this.btndelete.TabIndex = 10;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = true;
+            this.btndelete.Click += new System.EventHandler(this.btndelete_Click);
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.Location = new System.Drawing.Point(271, 17);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(75, 23);
+            this.btnsearch.TabIndex = 11;
+            this.btnsearch.Text = "Search";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            this.btnsearch.Click += new System.EventHandler(this.btnsearch_Click);
+            // 
+            // btnshowall
+            // 
+            this.btnshowall.Location = new System.Drawing.Point(485, 195);
+            this.btnshowall.Name = "btnshowall";
+            this.btnshowall.Size = new System.Drawing.Size(75, 23);
+            this.btnshowall.TabIndex = 12;
+            this.btnshowall.Text = "Show All";
+            this.btnshowall.UseVisualStyleBackColor = true;
+            this.btnshowall.Click += new System.EventHandler(this.btnshowall_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(410, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnshowall);
+            this.Controls.Add(this.btnsearch);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.txtpercentage);
+            this.Controls.Add(this.txtcity);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtrollno);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.TextBox txtrollno;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtcity;
+        private System.Windows.Forms.TextBox txtpercentage;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.Button btnshowall;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ AdoHomework/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+
+namespace AdoHomework
+{
+    public partial class Form1 : Form
+
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        public Form1()
+        {
+            InitializeComponent();
+            string str = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+            con = new SqlConnection(str);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "insert into student values(@nm,@city,@per)";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@nm", txtname.Text);
+                cmd.Parameters.AddWithValue("@city",txtcity.Text);
+                cmd.Parameters.AddWithValue("@per",Convert.ToDecimal( txtpercentage.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Record inserted");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = " update student set name=@nm,city=@city,percentage=@per where rollno=@rn";
+                cmd = new SqlCommand(qry, con);
+                cmd.Parameters.AddWithValue("@nm", txtname.Text);
+                cmd.Parameters.AddWithValue("@city", txtcity.Text);
+                cmd.Parameters.AddWithValue("@per", Convert.ToDecimal(txtpercentage.Text));
+                cmd.Parameters.AddWithValue("@rn", Convert.ToInt32(txtrollno.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Record updated");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btndelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = " delate from student where rollno=@rn";
+                cmd = new SqlCommand(qry, con); 
+                cmd.Parameters.AddWithValue("@rn", Convert.ToInt32(txtrollno.Text));
+                con.Open();
+                int result = cmd.ExecuteNonQuery();
+                if (result == 1)
+                {
+                    MessageBox.Show("Record deletedd");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnsearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "select * from student where rollno=@rn";
+                cmd = new SqlCommand(qry, con);
+                
+                cmd.Parameters.AddWithValue("@rn", Convert.ToInt32(txtrollno.Text));
+                con.Open();
+                dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while(dr.Read())
+                    {
+                        txtname.Text = dr["name"].ToString();
+                        txtcity.Text = dr["city"].ToString();
+                        txtpercentage.Text = dr["percentage"].ToString();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show( "Record not found");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void btnshowall_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string qry = "select * from student";
+                cmd = new SqlCommand(qry, con);
+                con.Open();
+                dr = cmd.ExecuteReader();
+                if(dr.HasRows)
+                {
+                    DataTable table = new DataTable();
+                    table.Load(dr);
+                    dataGridView1.DataSource = table;
+                }
+                else
+                {
+                    MessageBox.Show( "Record not found");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show( ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
